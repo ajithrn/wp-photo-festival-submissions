8957beef66a42cf486abdad3a59ac39751141aa6
Ajith R N <dev@ajithrn.com>
2024-02-03 03:21:17 +0530

    - initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# WordPress Photo Festival 2024 Submissions
+
+The Kerala WordPress Photo Festival is a global online photography competition organized by the WordPress Community of Kerala. This site will display the phots submitted using the hashtag #WPKeralaPhotos.s
+
+## Features
+
+- Display the submitted photos from the WordPress Kerala Photo Festival
+- Display photos according to specified tagId
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/ajithrn/wp-photo-festival-submissions.git`
+2. Navigate to the project directory: `cd wp-photo-festival-submissions`
+3. Open `index.html` in your web browser.
+
+
+## Technologies Used
+
+- HTML
+- CSS, SCSS
+- JavaScript
+
+## Credits
+
+- [WordPress](wordpress.org) for the logos and API used.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ assets/js/main.js
@@ -0,0 +1,87 @@
+/**
+ * WordPress Kerala Photo Festival Entries
+ * Ver: 1.0
+ * Author: Ajith
+ * URL: https://ajithrn.com
+ * JS
+ */
+// JavaScript code
+
+const galleryContainer = document.querySelector('.gallery');
+const tagId = 5852; // tag-id (WPKeralaPhotos) 
+let pageNumber = 1;
+
+
+function fetchPhotos() {
+  const apiUrl = `https://wordpress.org/photos/wp-json/wp/v2/photos/?photo-tags=${tagId}&page=${pageNumber}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((photos) => {
+      photos.forEach((photo) => {
+        // Create a new gallery item for each photo
+        const galleryItem = document.createElement('div');
+        galleryItem.className = 'gallery-item';
+
+        // Create an image element and set its source to the photo thumbnail URL
+        const img = document.createElement('img');
+        img.src = photo['photo-thumbnail-url'];
+
+        // Append the image to the gallery item
+        galleryItem.appendChild(img);
+
+        // create div for photo description
+        const itemDescription = document.createElement('div');
+        itemDescription.className = 'item-description';
+
+        // Append the item description to the gallery item
+        galleryItem.appendChild(itemDescription);
+
+        // Create a caption element and set its text to the photo caption
+        const caption = document.createElement('div');
+        caption.className = 'caption';
+        caption.innerHTML = photo['content']['rendered'];
+
+        // Append the caption to the gallery item
+        itemDescription.appendChild(caption);
+
+        // Create a download button element
+        const downloadButton = document.createElement('a');
+        downloadButton.className = 'view-button';
+        downloadButton.textContent = 'View in Photo Directory';
+        downloadButton.target = '_blank';
+        downloadButton.href = photo.link;
+
+        // Append the download button to the gallery item
+        itemDescription.appendChild(downloadButton);
+
+        // Append the gallery item to the gallery container
+        galleryContainer.appendChild(galleryItem);
+      });
+
+      // Increment the pageNumber for the next fetch
+      pageNumber++;
+    });
+}
+
+// Function to detect if user has scrolled to the bottom of the page
+function scrolledToBottom() {
+  const scrollTop =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const documentHeight =
+    document.documentElement.scrollHeight || document.body.scrollHeight;
+
+  return scrollTop + windowHeight >= documentHeight;
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', () => {
+  if (scrolledToBottom()) {
+    fetchPhotos();
+  }
+});
+
+// Initial fetch
+fetchPhotos();
